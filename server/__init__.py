541b1eaaea80ae2928e9ee0d9eb2ced912c2990f
sharmashubham850 <sharmashubham850@gmail.com>
2019-10-16 10:28:59 +0530

    Updated __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "6fbf401890e90b47598f5wv5l290eb21"
+
+# Change URI to your specific database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../test.db"
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
